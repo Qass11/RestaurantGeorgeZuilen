b47b62ef15eb1a8da41f8401e5d9f3c11e3dfdc6
Dani66864 <326358@student.mboutrecht.nl>
2021-09-28 13:04:20 +0200

    Update contact.php
    Opening hours added
    Footer added

--- contact.php
+++ contact.php
@@ -53,6 +53,88 @@
     Hoi ik ben Dani, dit is mijn contact pagina.
 </div>
 
+<div class="container border-bottom text-center opening-hours">
+        <h2>Opening Hours.</h2>
+        <p>Every working day you are welcome in our restaurant, below you can see all our opening hours.</p>
+        <div class="row">
+            <div class="col-sm-12 col-md">
+                <i class="fal fa-clock fa-fw icon-george"></i>
+                <p><strong>Maandag</strong></p>
+                <p>10:00 &mdash; 20:00</p>
+            </div>
+            <div class="col-sm-12 col-md">
+                <i class="fal fa-clock fa-fw icon-george"></i>
+                <p><strong>Dinsdag</strong></p>
+                <p>10:00 &mdash; 20:00</p>
+            </div>
+            <div class="col-sm-12 col-md">
+                <i class="fal fa-clock fa-fw icon-george"></i>
+                <p><strong>Woensdag</strong></p>
+                <p>10:00 &mdash; 20:00</p>
+            </div>
+            <div class="col-sm-12 col-md">
+                <i class="fal fa-clock fa-fw icon-george"></i>
+                <p><strong>Donderdag</strong></p>
+                <p>10:00 &mdash; 20:00</p>
+            </div>
+            <div class="col-sm-12 col-md">
+                <i class="fal fa-clock fa-fw icon-george"></i>
+                <p><strong>Vrijdag</strong></p>
+                <p>10:00 &mdash; 20:00</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container border-bottom text-center">
+        <div class="row">
+            <div class="col-sm-12 col-md socials">
+                <h2>Our Location.</h2>
+                <p>
+                    We are located in a beautiful location of the MBO Utrecht, near Utrecht Sciene Park, accessible by car, bicycle and public transport.
+                </p>
+                <div class="row">
+                    <div class="col-sm-12 col-md">
+                        <i class="fal fa-map-marked-alt fa-fw icon-george"></i>
+                        <strong>Location</strong>
+                        Daltonlaan 300, 3584 BK Utrecht
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md socials">
+                <h2>Our Socials.</h2>
+                <p>
+                    Always be the first to know about updates and actions at George Zuilen, follow our social media channels.
+                </p>
+                <div class="row">
+                    <div class="col-sm-12 col-md">
+                        <i class="fal fa-hashtag fa-fw icon-george"></i>
+                        <strong>Twitter</strong>
+                        @georgezuilen
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md socials">
+                <h2>Contact.</h2>
+                <p>
+                    Have questions or want to discuss certain opportunities with our team? Please feel free to contact us.
+                </p>
+                <div class="row">
+                    <div class="col-sm-12 col-md">
+                        <i class="fal fa-at fa-fw icon-george"></i>
+                        <strong>E-mail</strong>
+                        hello@georgezuilen.nl
+                    </div>
+
+                    <div class="col-sm-12 col-md">
+                        <i class="fal fa-phone-office fa-fw icon-george"></i>
+                        <strong>Phone</strong>
+                        030 12 34 56 7
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <div class="container no-border text-center">
     <p>&copy; 2021 George Zuilen</p>
 </div>

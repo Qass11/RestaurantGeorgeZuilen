116063153a3e7604929d563dc73afe30a1faf72b
mohamedloul1 <loulmohamed346@gmail.com>
2021-10-04 13:37:19 +0200

    Update about-us.php
    Created:
    - Information

--- about-us.php
+++ about-us.php
@@ -49,8 +49,59 @@
         </div>
     </div>
 
-    <div class="container border-bottom">
-        About us page.
+    <div class="container">
+        <div class="alert alert-dismissible fade show text-center" role="alert">
+            <a href="#">Click here for the latest covid-19 updates</a>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div class="container border-bottom about-us">
+        <div class="row">
+            <div class="col-12 col-md-7">
+                <h1>How the restaurant began</h1>
+                <p>
+                Many have come and gone, but George’s is still here, for 80 years and counting. The original building, consisting of the bar area with a small kitchen in back, began in 1930 as “Harry B’s,” named after owner Harry Urmeister. Harry was a founding member of the Nose Brothers organization, a long-standing Baylor tradition. An avid sportsman, he developed a strong relationship with Baylor athletics, pairing the two together for all these years.
+
+George Betros bought Harry B’s in 1966 and changed the name to “George’s.” It was at this time that our most famous drink, the “Big O™,” received its name. A waitress by the name of Lucille Brown called them Big Oranges, the rest of the staff shortened it to “Big O™,” and it stuck!</p>
+
+                </p>
+            </div>
+            <div class="col-12 col-md-5">
+                <img src="./src/img/eigennaar.jpg" class="about-image">
+            </div>
+        </div>
+    </div>
+
+    <div class="container border-bottom about-us">
+        <div class="row">
+        <div class="col-12 col-md-5">
+                <img src="./src/img/22222.jpg" class="about-image">
+            </div>
+            <div class="col-12 col-md-7">
+                <h1>What the mission is</h1>
+                <p>
+                Serving you iconic dishes and timeless classics with New York, Paris, the Côte d'Azur and Hong Kong in mind, along with local ingredients, freshly roasted coffee and signature cocktails, our restaurants are open all day, every day –  so come join us for an early morning espresso, a lazy Sunday brunch or an intimate dinner with friends in a relaxed, elegant atmosphere.
+                </p>
+            </div>
+        
+        </div>
+    </div>
+
+    <div class="container border-bottom about-us">
+        <div class="row">
+            <div class="col-12 col-md-7">
+                <h1>The recent success</h1>
+                <p>
+                Most of our restaurants are designed by Amsterdam based Architecture & Design studio Framework, who signed for our bespoke interiors and sophisticated yet intimate ambiance, featuring typical George elements like metro tiles, lush leather couches, brass lighting, and marble bars. With each location having its own charm, your visit to George quite literally is a feast for your eyes and taste buds.
+
+                          Most of our locations have stunning, sunny terraces and are open all day, every day for breakfast, lunch and dinner.
+                </p>
+            </div>
+            <div class="col-12 col-md-5">
+                <img src="./src/img/3333333.jpg" class="about-image">
+            </div>
+        </div>
     </div>
 
     <div class="container no-border text-center">

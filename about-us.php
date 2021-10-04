2b42dcf6acc5166b385a95f30043d96697456624
Qass11 <78322136+Qass11@users.noreply.github.com>
2021-10-04 14:46:40 +0200

    Update about-us.php
    Fixed the modal

--- about-us.php
+++ about-us.php
@@ -34,7 +34,7 @@
                 <a class="nav-link" href="./book-event.php"><i class="fal fa-glass-cheers fa-fw"></i> Events</a>
             </li>
             <li>
-                <a class="nav-link" href="#"><i class="fal fa-calendar-plus fa-fw"></i> Book a table</a>
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#bookATable"><i class="fal fa-calendar-plus fa-fw"></i> Book a table</a>
             </li>
             <li>
                 <a class="nav-link" href="./contact.php"><i class="fal fa-paper-plane fa-fw"></i> Contact</a>

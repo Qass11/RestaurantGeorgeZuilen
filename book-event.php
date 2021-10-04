4949df018fd719751d9696533886df749932e201
Mitchel Jansen <mitchel@mexter.nl>
2021-10-04 11:46:55 +0200

    Update book-event.php
    Created:
    - Call To Action
    - Information text
    - Space for some rooms

--- book-event.php
+++ book-event.php
@@ -11,54 +11,82 @@
 </head>
 <body>
 
-<!--<div class="loader" id="loader">-->
-<!--    <div class="text-loading" id="preloader">-->
-<!--        <h3><span>George</span> Zuilen</h3>-->
-<!--    </div>-->
-<!--</div>-->
+    <!--<div class="loader" id="loader">-->
+    <!--    <div class="text-loading" id="preloader">-->
+    <!--        <h3><span>George</span> Zuilen</h3>-->
+    <!--    </div>-->
+    <!--</div>-->
 
-<div class="container border-bottom header-bar">
-    <a href="#" class="george-logo">George</a>
+    <div class="container border-bottom header-bar">
+        <a href="#" class="george-logo">George</a>
 
-    <ul>
-        <li>
-            <a class="nav-link" href="./index.php"><i class="fal fa-home fa-fw"></i> Home</a>
-        </li>
-        <li>
-            <a class="nav-link" href="./about-us.php"><i class="fal fa-users fa-fw"></i> About us</a>
-        </li>
-        <li>
-            <a class="nav-link" href="./menu.php"><i class="fal fa-utensils-alt fa-fw"></i> Menu</a>
-        </li>
-        <li>
-            <a class="nav-link" href="./book-event.php"><i class="fal fa-glass-cheers fa-fw"></i> Events</a>
-        </li>
-        <li>
-            <a class="nav-link" href="#"><i class="fal fa-calendar-plus fa-fw"></i> Book a table</a>
-        </li>
-        <li>
-            <a class="nav-link" href="./contact.php"><i class="fal fa-paper-plane fa-fw"></i> Contact</a>
-        </li>
-    </ul>
-</div>
-
-<div class="container">
-    <div class="alert alert-dismissible fade show text-center" role="alert">
-        <a href="#">Click here for the latest covid-19 updates</a>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <ul>
+            <li>
+                <a class="nav-link" href="./index.php"><i class="fal fa-home fa-fw"></i> Home</a>
+            </li>
+            <li>
+                <a class="nav-link" href="./about-us.php"><i class="fal fa-users fa-fw"></i> About us</a>
+            </li>
+            <li>
+                <a class="nav-link" href="./menu.php"><i class="fal fa-utensils-alt fa-fw"></i> Menu</a>
+            </li>
+            <li>
+                <a class="nav-link" href="./book-event.php"><i class="fal fa-glass-cheers fa-fw"></i> Events</a>
+            </li>
+            <li>
+                <a class="nav-link" href="#"><i class="fal fa-calendar-plus fa-fw"></i> Book a table</a>
+            </li>
+            <li>
+                <a class="nav-link" href="./contact.php"><i class="fal fa-paper-plane fa-fw"></i> Contact</a>
+            </li>
+        </ul>
     </div>
-</div>
 
-<div class="container border-bottom">
-    Book event page
-</div>
+    <div class="container no-border call-to-action">
+        <div class="call-to-action-menu-one">
+            <div class="text-place">
+                <h2>Your guests will <i class="fal fa-heart fa-fw"></i> George</h2>
+            </div>
+        </div>
+    </div>
 
-<div class="container no-border text-center">
-    <p>&copy; 2021 George Zuilen</p>
-</div>
+    <div class="container border-bottom about-us text-center">
+        <div class="row">
+            <div class="col-12">
+                <h1>ANYTHING IS POSSIBLE: JUST ASK US ABOUT THE POSSIBILITIES.</h1>
+                <p>
+                    Over the years, we've hosted a variety of events. From private birthday parties, PR-events and exclusive product launches, to corporate events, luncheons and walking dinners for large groups. So whether you want to celebrate in style or have a business meeting that is anything but ordinary, George will offer you just that – and more. Our venues come in various sizes, each with their own style. With great food & beverages options, top-notch facilities and an amazing crew that is highly experienced.
+                </p>
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-<script src="./src/js/main.js"></script>
+                <p>
+                    Hosting a private party for a special occasion? Your guests will love cosy Georgette at the P.C. Hooftstraat. Organizing a corporate event? Then George Marina is the perfect choice. Planning a press event or influencer lunch to boost your brand? George W.P.A. is an ideal, comfy spot. Either way, no matter which location you choose, we'll make sure you and your guests feel right at home.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container border-bottom">
+        Meeting rooms
+    </div>
+
+    <div class="container border-bottom">
+        EXCLUSIVE RENTAL OF RESTAURANT & TERRACE
+    </div>
+
+    <div class="container border-bottom">
+        Some menus
+    </div>
+
+    <div class="container border-bottom">
+        Some extra photos
+    </div>
+
+    <div class="container no-border text-center">
+        <p>&copy; 2021 George Zuilen</p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <script src="./src/js/main.js"></script>
 </body>
 </html>

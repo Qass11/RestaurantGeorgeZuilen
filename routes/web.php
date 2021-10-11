2bb84d0264abd9ab6ace683c387bbb364af6c105
mohamedloul1 <loulmohamed346@gmail.com>
2021-10-11 15:36:55 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CoronaController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\MenuController;
@@ -12,3 +13,5 @@ Route::get('about', [AboutController::class, 'index'])->name('about');
 Route::get('menu', [MenuController::class, 'index'])->name('menu');
 Route::get('event', [EventController::class, 'index'])->name('event');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
+
+Route::get('corona', [CoronaController::class, 'index'])->name('corona');

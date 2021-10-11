e65c75e484e7ab98707b4f3b9e4fc216804185c7
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-11 19:44:55 +0200

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,37 +4,36 @@
 @section('content')
     <div class="container border-bottom about-us">
         <div class="row">
-            <div class="col-12 col-md-7">
-                <h1>How the restaurant began</h1>
-                <p>Many have come and gone, but George’s is still here, for 80 years and counting. The original building, consisting of the bar area with a small kitchen in back, began in 1930 as “Harry B’s,” named after owner Harry Urmeister. Harry was a founding member of the Nose Brothers organization, a long-standing Baylor tradition. An avid sportsman, he developed a strong relationship with Baylor athletics, pairing the two together for all these years. George Betros bought Harry B’s in 1966 and changed the name to “George’s.” It was at this time that our most famous drink, the “Big O™,” received its name. A waitress by the name of Lucille Brown called them Big Oranges, the rest of the staff shortened it to “Big O™,” and it stuck!</p>
-            </div>
-            <div class="col-12 col-md-5">
-                <img src="{{ asset('images/about-one.jpg') }}" class="about-image" alt="How the restaurant began">
-            </div>
-        </div>
-    </div>
-
-    <div class="container border-bottom about-us">
-        <div class="row">
-            <div class="col-12 col-md-5">
-                <img src="{{ asset('images/about-two.jpg') }}" class="about-image" alt="What is our mission?">
-            </div>
-            <div class="col-12 col-md-7">
-                <h1>What is our mission?</h1>
-                <p>Serving you iconic dishes and timeless classics with New York, Paris, the Côte d'Azur and Hong Kong in mind, along with local ingredients, freshly roasted coffee and signature cocktails, our restaurants are open all day, every day –  so come join us for an early morning espresso, a lazy Sunday brunch or an intimate dinner with friends in a relaxed, elegant atmosphere.</p>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <img src="{{ asset('images/about-one.jpg') }}" class="about-image" alt="The recent success">
+                <h2>Our Start.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium alias dolorum illo laborum porro unde veritatis voluptate? Alias atque autem blanditiis impedit ipsa quidem sed sint totam veniam voluptate!
+                </p>
             </div>
 
-        </div>
-    </div>
-
-    <div class="container border-bottom about-us">
-        <div class="row">
-            <div class="col-12 col-md-7">
-                <h1>The recent success</h1>
-                <p>Most of our restaurants are designed by Amsterdam based Architecture & Design studio Framework, who signed for our bespoke interiors and sophisticated yet intimate ambiance, featuring typical George elements like metro tiles, lush leather couches, brass lighting, and marble bars. With each location having its own charm, your visit to George quite literally is a feast for your eyes and taste buds.  Most of our locations have stunning, sunny terraces and are open all day, every day for breakfast, lunch and dinner.</p>
+            <div class="col-sm-12 col-md-6 mb-3">
+                <img src="{{ asset('images/about-two.jpg') }}" class="about-image" alt="The recent success">
+                <h2>Our Mission.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium alias dolorum illo laborum porro unde veritatis voluptate? Alias atque autem blanditiis impedit ipsa quidem sed sint totam veniam voluptate!
+                </p>
             </div>
-            <div class="col-12 col-md-5">
+
+            <div class="col-sm-12 col-md-6">
                 <img src="{{ asset('images/about-three.jpeg') }}" class="about-image" alt="The recent success">
+                <h2>Our Success.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium alias dolorum illo laborum porro unde veritatis voluptate? Alias atque autem blanditiis impedit ipsa quidem sed sint totam veniam voluptate!
+                </p>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+                <img src="{{ asset('images/about-four.jpg') }}" class="about-image" alt="The recent success">
+                <h2>MBO Utrecht.</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium alias dolorum illo laborum porro unde veritatis voluptate? Alias atque autem blanditiis impedit ipsa quidem sed sint totam veniam voluptate!
+                </p>
             </div>
         </div>
     </div>

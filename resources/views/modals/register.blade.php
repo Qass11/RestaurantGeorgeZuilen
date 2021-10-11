013be8069bd2757e00945526054b7257cb808b29
Dani66864 <326358@student.mboutrecht.nl>
2021-10-11 15:44:00 +0200

    Create register.blade.php

---
+++ resources/views/modals/register.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="registerGeorge" tabindex="-1" aria-labelledby="registerGeorgeLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="registerGeorgeLabel">Register at George</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <img src="{{ asset('images/booktable.jpg') }}" class="img-fluid image-book">
+            <div class="modal-body">
+                register
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>

2289524720b94896697f2d5a6bc75e84106ff270
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-26 00:54:58 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,15 @@ Route::get('about', [AboutController::class, 'index'])->name('about');
 Route::get('menu', [MenuController::class, 'index'])->name('menu');
 Route::get('event', [EventController::class, 'index'])->name('event');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
-
-
 Route::get('careers', [CareerController::class, 'index'])->name('career');
 Route::get('gallery', [GalleryController::class, 'index'])->name('gallery');
 
-// Auth
-
-Route::get('create_account', [AuthController::class, 'createRegister'])->name('create')->middleware('guest'); // Register page
+Route::get('create_account', [AuthController::class, 'createRegister'])->name('createRegister')->middleware('guest'); // Register page
 Route::post('create_account', [AuthController::class, 'storeRegister'])->middleware('guest'); // Send the register form
 
-Route::get('login', [AuthController::class, 'createLogin'])->middleware('guest'); // Login page
+Route::get('login', [AuthController::class, 'createLogin'])->name('createLogin')->middleware('guest'); // Login page
 Route::post('login', [AuthController::class, 'storeLogin'])->middleware('guest'); // Send the login form
 
 Route::post('logout', [AuthController::class, 'destroy'])->middleware('auth'); // Log out if logged in.
+
+Route::get('activate', [AuthController::class, 'createActivate'])->name('createActivate')->middleware('guest'); // Activate page

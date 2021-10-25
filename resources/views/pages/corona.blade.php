1ae05da9b69f4cbeededf47bcd65866c188a3cda
mohamedloul1 <loulmohamed346@gmail.com>
2021-10-25 21:48:53 +0200

    corona uodate
    Add corrona rules

--- resources/views/pages/corona.blade.php
+++ resources/views/pages/corona.blade.php
@@ -3,7 +3,37 @@
 
 @section('content')
     <div class="container border-bottom">
-    corona update
+        <div class="corona-one">
+            <div class="text-corona">
+                <h2>COVID-19 UPDATE</h2>
 
+            </div>
+        </div>
     </div>
+
+<div class="container border-bottom">
+    <div class="corona-text">
+        <h1 class="text-center">While indoor dining and private events are limited for now, George loves bringing people together in the safest way. </h1>
+        <h1 class="text-center">All of our kitchen and waiting staff are following the HACCP regulations for food preparation, hygiene, and handling. </h1>
+        <h1 class="text-center">PS: Never forget, George wants what’s best for you! <img src="https://uploads-ssl.webflow.com/5f3e6fbcc9fdf842f6add861/6089746b4792a9cce4093e31_Heart.svg" loading="lazy" alt="social media love icon" class="image-8"> </h1>
+    </div>
+</div>
+<div class="container border-bottom">
+    <div class="text-corona-hous-rules">
+        <h2>Please take a minute to read our new ‘House Rules’</h2>
+        <div class="text-rules">
+        <p>There will be a host at the front door taking details for contact tracing purposes</p>
+            <p>We will seat you on arrival</p>
+            <p>You will remain spaced at a 1.5 m distance</p>
+            <p>Tables are sanitised before and after your visit</p>
+            <p>We prefer contactless payment options</p>
+          </div>
+       </div>
+    </div>
+
+
+
+
+
+
 @endsection

0c796f1efe524ea57f7db19ed9fa73f06d7c9640
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-25 22:34:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\GalleryController;
@@ -18,3 +19,8 @@ Route::get('contact', [ContactController::class, 'index'])->name('contact');
 
 Route::get('careers', [CareerController::class, 'index'])->name('career');
 Route::get('gallery', [GalleryController::class, 'index'])->name('gallery');
+
+// Auth
+
+Route::get('create_account', [AuthController::class, 'create'])->name('create'); // View the page
+Route::post('create_account', [AuthController::class, 'store']); // Send the form

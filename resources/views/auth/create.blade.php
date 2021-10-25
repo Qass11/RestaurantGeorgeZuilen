ba68271286de07cd121505a4f9893ec1ba6f9bf3
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-25 23:56:19 +0200

    Update create.blade.php

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -4,19 +4,44 @@
 @section('content')
 <div class="container border-bottom">
     <form action="/create_account" method="POST">
+
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
         @csrf
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Firstname</label>
-            <input type="text" class="form-control" id="firstname" name="firstname" required>
+            <input type="text" class="form-control" id="firstname" name="firstname" value="{{ old('firstname') }}" required>
+            @error('firstname')
+                <p>{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Lastname</label>
-            <input type="text" class="form-control" id="lastname" name="lastname" required>
+            <input type="text" class="form-control" id="lastname" name="lastname" value="{{ old('lastname') }}" required>
+            @error('lastname')
+                <p>{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+            @error('email')
+                <p>{{ $message }}</p>
+            @enderror
         </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+                @error('password')
+                <p>{{ $message }}</p>
+                @enderror
+            </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>

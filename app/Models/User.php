42096c63b14b8d4d3caf12974fd99b92b8546ea1
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-25 22:34:40 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,18 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
+     * @var array
+     */
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
      * @var string[]
      */
     protected $guarded = [];
@@ -37,4 +49,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }

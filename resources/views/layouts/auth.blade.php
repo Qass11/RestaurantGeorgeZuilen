340732e9a98f738b534093ba99cd9e4d546ff4dd
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-26 10:26:49 +0200

    Create auth.blade.php

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('pagename') &mdash; {{ config('app.name') }}</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.2/css/all.css" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+</head>
+
+<body class="bg-blue">
+
+    @yield('content')
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+
+</html>

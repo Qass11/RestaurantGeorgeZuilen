c94f0bc475dec34f19684a02a8e8ec4c3ddaec3a
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-27 20:39:04 +0200

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use App\Notifications\sendContactNotification;
+use App\Notifications\sendCopyContactNotification;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -9,5 +12,24 @@ class ContactController extends Controller
     public function index()
     {
         return view('pages.contact');
+    }
+
+    public function storeContact()
+    {
+        $attributes = request()->validate([
+           'firstname'              => ['required', 'min:3', 'max:255'],
+           'lastname'               => ['required', 'min:3', 'max:255'],
+           'email'                  => ['email', 'required', 'min:3', 'max:255'],
+           'phone_number'           => ['required', 'min:3', 'max:255'],
+           'subject'                => ['required', 'min:3', 'max:255'],
+           'text'                   => ['required', 'min:3'],
+        ]);
+
+        $email = 'admin@georgezuilen.nl';
+        $contact = Contact::create($attributes);
+        $contact->notify(new sendCopyContactNotification($contact));
+        $contact->notify(new sendContactNotification($contact, $email));
+
+        return redirect('/contact')->with('success', 'The completed form has been sent to the team of George Zuilen.');
     }
 }

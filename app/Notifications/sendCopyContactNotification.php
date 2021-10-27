b5d2692657930625ea2d6b2aeeb0317d7c2ced1e
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-27 20:52:38 +0200

    Update sendCopyContactNotification.php

--- app/Notifications/sendCopyContactNotification.php
+++ app/Notifications/sendCopyContactNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 
 class sendCopyContactNotification extends Notification
 {
@@ -47,10 +48,10 @@ class sendCopyContactNotification extends Notification
             ->greeting('Hello ' . $this->data->firstname . '!')
             ->line('We hereby send you a copy of your completed contact form on the Restaurant George Zuilen website.')
             ->line('The information below is given:')
-            ->line('E-mail: ' . $this->data->email)
-            ->line('Phone number: ' . $this->data->phone_number)
-            ->line('Subject: ' . $this->data->subject)
-            ->line('Message: ' . $this->data->text)
+            ->line(new HtmlString('<strong>E-mail:</strong><br/>' . $this->data->email))
+            ->line(new HtmlString('<strong>Phone number:</strong><br/>' . $this->data->phone_number))
+            ->line(new HtmlString('<strong>Subject:</strong><br/>' . $this->data->subject))
+            ->line(new HtmlString('<strong>Message:</strong><br/>' . $this->data->text))
             ->action('Reserve a table in our restaurant', route('index'));
     }
 

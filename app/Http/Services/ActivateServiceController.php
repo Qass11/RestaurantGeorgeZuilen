c7e19aeb6e42e6d4168b732df4de885009626d33
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-27 23:11:39 +0200

    Update ActivateServiceController.php

--- app/Http/Services/ActivateServiceController.php
+++ app/Http/Services/ActivateServiceController.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace App\Http\Services;
+
+class ActivateServiceController {
+
+    public function address($id, $streetname, $house_number, $zipcode, $city) {
+
+    }
+
+    public function student($id, $user_id, $address_id, $student_number) {
+
+    }
+
+    public function employee($id, $user_id, $address_id, $employee_number) {
+
+    }
+
+    public function user($id, $email_verified_at, $phone_number) {
+
+    }
+}

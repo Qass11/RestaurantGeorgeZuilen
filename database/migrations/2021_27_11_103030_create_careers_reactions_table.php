127cf5d215abf7e81726369d3dc16deadb7e9212
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-27 19:06:11 +0200

    Create 2021_27_11_103030_create_careers_reactions_table.php

---
+++ database/migrations/2021_27_11_103030_create_careers_reactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('careers_reactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('position');
+            $table->longText('motivation');
+            $table->string('cv_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('careers_reactions');
+    }
+};

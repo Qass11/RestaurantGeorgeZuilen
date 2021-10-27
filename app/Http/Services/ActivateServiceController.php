8b0c7747e7c27314bb98f5c5627bee3e3c8414d4
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-28 00:39:34 +0200

    Update ActivateServiceController.php

--- app/Http/Services/ActivateServiceController.php
+++ app/Http/Services/ActivateServiceController.php
@@ -8,17 +8,21 @@ class ActivateServiceController {
 
     public function address(array $data) {
         $this->attributes = $data;
+        return $this->attributes;
     }
 
     public function student(array $data) {
         $this->attributes = $data;
+        return $this->attributes;
     }
 
     public function employee(array $data) {
         $this->attributes = $data;
+        return $this->attributes;
     }
 
     public function user(array $data) {
         $this->attributes = $data;
+        return $this->attributes;
     }
 }

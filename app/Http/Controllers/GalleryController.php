4d3d32e62a1f0c69f8c72fc1caadca24643d6e54
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-31 22:12:45 +0100

    Update GalleryController.php

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
     public function index()
     {
-        return view('pages.gallery');
+
+        $gallery = Gallery::all();
+        return view('pages.gallery', compact('gallery'));
     }
 }

634f6d930359911beab3c8abae45fbb902e702f8
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-31 20:58:19 +0100

    Create 2021_31_11_205230_create_opening_hours_table.php

---
+++ database/migrations/2021_31_11_205230_create_opening_hours_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opening_hours', function (Blueprint $table) {
+            $table->id();
+            $table->string('day');
+            $table->string('begin');
+            $table->string('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opening_hours');
+    }
+};

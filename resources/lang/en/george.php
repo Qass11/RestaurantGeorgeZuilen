134aeaf96b1b7042a0a3035b7daddf1c18d7832d
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-10-31 22:12:58 +0100

    Update george.php

--- resources/lang/en/george.php
+++ resources/lang/en/george.php
@@ -23,4 +23,10 @@ return [
         'title'             => 'Opening Hours.',
         'description'       => 'Every working day you are welcome in our restaurant, below you can see all our opening hours.',
     ],
+
+    'gallery' => [
+        'title'             => 'Our Beauty.',
+        'description'       => 'Below you\'ll see our gallery of photos we\'re proud of. This gallery can give you an impression about your visit to George Zuilen.',
+        'nothing'           => 'Currently there are no gallery entries published yet.',
+    ],
 ];

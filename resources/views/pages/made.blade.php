4113677902dc76d7798644301c834d57ec8ccd48
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-14 20:41:20 +0100

    Fixed the Made page.

--- resources/views/pages/made.blade.php
+++ resources/views/pages/made.blade.php
@@ -1,41 +1,36 @@
 @extends('layouts.app')
-@section('pagename', 'Home')
+@section('pagename', 'Our Developers')
 
 @section('content')
+    <div class="container border-bottom text-center">
+        <h1>Our Developers</h1>
+        <p class="bottom-space">
+            Below is our team of Developers at Restaurant George Zuilen.
+        </p>
+        <div class="row">
+            <div class="col-sm-4 col-md">
+                <img src="{{ asset('images/test-image.jpg') }}" class="img-fluid employee">
+                <h5 class="empname">Dani al Jumaat</h4>
+                    <p class="function">Developer</p>
+            </div>
 
-    <footer>
-        <p id="logo">Mboutrecht-Ict-Acadimie</p>
-    </footer>
-    <div class="container mt-3 img-header">
-    <div class="row mb-3">
-        <div class="col-sm-12 col-md-12">
-            <strong class="style-letter">MBO</strong>
-        </div>
-        <div class="col-sm-12 col-md-12 text-center">
-            <strong class="style-letter">UTRECHT</strong>
-        </div>
-        <div class="col-sm-12 col-md-12 text-end">
-            <strong class="style-letter">2021</strong>
-        </div>
-    </div>
-    </div>
+            <div class="col-sm-4 col-md">
+                <img src="{{ asset('images/Mohammed.jpg') }}" class="img-fluid employee">
+                <h5 class="empname">Mohammed Loul</h4>
+                    <p class="function">Developer</p>
+            </div>
 
-    <div class="row text-center">
-        <div class="col-sm-12 col-md-12 mt-4 mb-5 naam">
-            <a href="#"><h2 class="zicht">Dani Al Jumaat</h2></a>
-            <img src="{{asset('/images/test-image.jpg')}}">
-        </div>
-        <div class="col-sm-12 col-md-12 mb-5 naam">
-            <a href="#"><h2 class="zicht">Mohammed Loul</h2></a>
-            <img src="{{asset("/images/Mohammed.jpg")}}">
-        </div>
-        <div class="col-sm-12 col-md-12 mb-5 naam">
-            <a href="#"><h2 class="zicht">Quassim kurdi</h2></a>
-            <img src="{{asset('/images/Qassim.jpeg')}}">
-        </div>
-        <div class="col-sm-12 col-md-12 mb-5 naam">
-            <a href="#"><h2 class="zicht">Mitchel Jansen</h2></a>
-            <img src="./img/test.jpeg">
+            <div class="col-sm-4 col-md">
+                <img src="{{ asset('images/Qassim.jpeg') }}" class="img-fluid employee">
+                <h5 class="empname">Qassim Kurdi</h4>
+                    <p class="function">Developer</p>
+            </div>
+
+            <div class="col-sm-4 col-md">
+                <img src="{{ asset('images/mitchel.jpg') }}" class="img-fluid employee">
+                <h5 class="empname">Mitchel Jansen</h4>
+                    <p class="function">Developer</p>
+            </div>
         </div>
     </div>
-    @endsection
+ @endsection

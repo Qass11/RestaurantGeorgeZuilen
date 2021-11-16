1d24f151283c5e45580c261438712db3d2a6808a
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-16 15:09:00 +0100

    Update CoursesController.php

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -20,6 +20,7 @@ class CoursesController extends Controller
     {
         if($request->isMethod('post')) {
             // Hier je form opbouwen Dani & Mo.
+            // Niet verwijderen.
         }
 
         return view('app.courses.subscribe');

b765546ada739a42e36688b063db94c2c4e7d311
Qass11 <78322136+Qass11@users.noreply.github.com>
2021-11-16 20:29:34 +0100

    Update overview.blade.php

--- resources/views/app/reservations/overview.blade.php
+++ resources/views/app/reservations/overview.blade.php
@@ -3,20 +3,41 @@
 
 @section('content')
     <div class="container border-bottom">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">type</th>
+                <th scope="col">persons</th>
+                <th scope="col">date</th>
+                <th scope="col">time</th>
+                <th scope="col">firstname</th>
+                <th scope="col">lastname</th>
+                <th scope="col">email</th>
+                <th scope="col">phone_numebr</th>
+                <th scope="col">comments</th>
+                <th scope="col">advertising</th>
+            </tr>
+            </thead>
+            <tbody>
         @forelse($reservations as $reservation)
-            {{ $reservation->id }}
-            {{ $reservation->type }}
-            {{ $reservation->persons }}
-            {{ $reservation->date }}
-            {{ $reservation->time }}
-            {{ $reservation->firstname }}
-            {{ $reservation->lastname }}
-            {{ $reservation->email }}
-            {{ $reservation->phone_number }}
-            {{ $reservation->comments }}
-            {{ $reservation->advertising }}
+            <tr>
+                <th scope="row">{{ $reservation->id }}</th>
+                <td>{{ $reservation->type }}</td>
+                <td>{{ $reservation->persons }}</td>
+                <td>{{ $reservation->date }}</td>
+                <td>{{ $reservation->time }}</td>
+                <td>{{ $reservation->firstname }}</td>
+                <td>{{ $reservation->lastname }}</td>
+                <td>{{ $reservation->email }}</td>
+                <td>{{ $reservation->phone_number }}</td>
+                <td>{{ $reservation->comments }}</td>
+                <td>{{ $reservation->advertising }}</td>
+            </tr>
         @empty
             Nothing to show
         @endforelse
+                </tbody>
+            </table>
     </div>
 @endsection

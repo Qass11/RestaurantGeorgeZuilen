e299e93e060d7ae79f675883c98e5d048d0a2438
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-16 17:51:35 +0100

    Create Reservation.php

---
+++ app/Models/Reservation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Reservation extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'reservations';
+
+    protected $fillable = [
+        'type',
+        'persons',
+        'date',
+        'time',
+        'firstname',
+        'lastname',
+        'email',
+        'phone_number',
+        'comments'
+    ];
+}

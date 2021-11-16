93c0e09d38fcc85b90595df0da3d0f55ec4730fa
Dani66864 <326358@student.mboutrecht.nl>
2021-11-16 13:40:56 +0100

    Madepage verbeterd
    Hierbij heb ik animatie toegevoegd aan de afbeelding

--- resources/views/pages/made.blade.php
+++ resources/views/pages/made.blade.php
@@ -2,6 +2,20 @@
 @section('pagename', 'Home')
 
 @section('content')
+<<<<<<< Updated upstream
+=======
+    <div class="container border-bottom text-center controller">
+        <h1>Our Developers</h1>
+        <p class="bottom-space">
+            Below is our team of Developers at Restaurant George Zuilen.
+        </p>
+        <div class="row">
+            <div class="col-sm-4 col-md">
+                <img src="{{ asset('images/test-image.jpg') }}" class="img-fluid employee">
+                <h5 class="empname">Dani al Jumaat</h4>
+                    <p class="function">Developer</p>
+            </div>
+>>>>>>> Stashed changes
 
     <footer>
         <p id="logo">Mboutrecht-Ict-Acadimie</p>

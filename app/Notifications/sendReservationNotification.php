431b90ddf5f677c1b77f3043facb25878df41091
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-11-16 17:57:30 +0100

    Update sendReservationNotification.php

--- app/Notifications/sendReservationNotification.php
+++ app/Notifications/sendReservationNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 
 class sendReservationNotification extends Notification
 {
@@ -43,9 +44,17 @@ class sendReservationNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject('Copy of your reservation.')
-            ->greeting('Hello ' . $this->data->firstname . '!')
-            ->line('Confirmation');
+            ->subject('Confirmation of your reservation at George Zuilen')
+            ->greeting('Hello ' . $this->data->firstname . ' ' . $this->data->lastname .'!')
+            ->line('You have just successfully made a reservation at Restaurant George Zuilen! The information below is given:')
+            ->line(new HtmlString('<strong>Date:</strong> ' . $this->data->date))
+            ->line(new HtmlString('<strong>Time:</strong> ' . $this->data->time))
+            ->line(new HtmlString('<strong>Type:</strong> ' . $this->data->type))
+            ->line(new HtmlString('<strong>Persons:</strong> ' . $this->data->persons))
+            ->line(new HtmlString('<strong>Email:</strong> ' . $this->data->email))
+            ->line(new HtmlString('<strong>Phone:</strong> ' . $this->data->phone_number))
+            ->line(new HtmlString('<strong>Comments:</strong> ' . $this->data->comments))
+            ->line('We look forward to seeing you soon at our restaurant!');
     }
 
     /**

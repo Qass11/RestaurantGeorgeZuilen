caeccfef2a87270fd89b9557a8e398025b10fc69
Qass11 <78322136+Qass11@users.noreply.github.com>
2021-11-16 19:57:45 +0100

    Update 2021_10_11_103030_create_reservations_table.php

--- database/migrations/2021_10_11_103030_create_reservations_table.php
+++ database/migrations/2021_10_11_103030_create_reservations_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone_number');
             $table->longText('comments')->nullable();
+            $table->string('advertising');
             $table->timestamps();
         });
     }

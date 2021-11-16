5c36888228cee036c0cdd10d658f282d86b1a669
Qass11 <78322136+Qass11@users.noreply.github.com>
2021-11-16 20:20:17 +0100

    Update overview.blade.php

--- resources/views/app/reservations/overview.blade.php
+++ resources/views/app/reservations/overview.blade.php
@@ -6,6 +6,15 @@
         @forelse($reservations as $reservation)
             {{ $reservation->id }}
             {{ $reservation->type }}
+            {{ $reservation->persons }}
+            {{ $reservation->date }}
+            {{ $reservation->time }}
+            {{ $reservation->firstname }}
+            {{ $reservation->lastname }}
+            {{ $reservation->email }}
+            {{ $reservation->phone_number }}
+            {{ $reservation->comments }}
+            {{ $reservation->advertising }}
         @empty
             Nothing to show
         @endforelse

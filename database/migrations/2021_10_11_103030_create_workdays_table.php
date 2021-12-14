4c72e6dfbab760a50f7dbbe1c6e9a7600dbcc74f
Mitchel Jansen <324712@student.mboutrecht.nl>
2021-12-14 16:56:59 +0100

    Create 2021_10_11_103030_create_workdays_table.php

---
+++ database/migrations/2021_10_11_103030_create_workdays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workdays', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id');
+            $table->dateTime('date');
+            $table->string('time_start');
+            $table->string('time_stop');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workdays');
+    }
+};
